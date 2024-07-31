f552eceb175900ad61d61097bf6b4347bcf0cb07
prashanth.kanukuntla <prashanth.kanukuntla@italentdigital.com>
2024-07-31 20:29:43 +0530

    add insufficient funds class

---
+++ src/main/java/InsufficientFundsException.java
@@ -0,0 +1,23 @@
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException() {
+        super();
+    }
+
+    public InsufficientFundsException(String str) {
+        super(str);
+    }
+
+    public InsufficientFundsException(String str, Throwable throwable) {
+        super(str, throwable);
+    }
+
+    public InsufficientFundsException(Throwable throwable) {
+        super(throwable);
+    }
+
+}
+
+
+
+
+
